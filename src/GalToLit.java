59c3af2bd7fb781f7721e1ff309dade76db63456
lehRaphaela <alexiaraphela314@gmail.com>
2024-11-23 11:39:29 -0300

    New coment

---
+++ src/GalToLit.java
@@ -0,0 +1,16 @@
+
+
+public class GalToLit {
+    
+    
+    public static void main(String[] args) {
+        
+        // declare as variaveis
+        double gallons=10;
+        double liters=1;
+        // adicione calculo aqui
+        
+        //exiba o resultado para i usuario
+        System.out.println(gallons+" gallons equal "+liters+" liters");
+    }
+}
